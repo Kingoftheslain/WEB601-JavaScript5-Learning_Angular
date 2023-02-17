b36c20a63bf4c82f2ed9c7abc3f7e80e2d925de9
Michael MacNeill <98714598+Kingoftheslain@users.noreply.github.com>
2023-02-16 23:22:14 -0500

    Finished Assignment 4
    Finished all the elements of Assignment 4

--- Mic_MacNeill_WEB601Assignments_MyHobbies/src/app/app.module.ts
+++ Mic_MacNeill_WEB601Assignments_MyHobbies/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HobbyComponent } from './hobby/hobby.component';
 import { ContentListComponent } from './content-list/content-list.component';
 import { FilterByTypePipe } from './filter-by-type.pipe';
+import { HoverAffectDirective } from './hover-affect.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     HobbyComponent,
     ContentListComponent,
-    FilterByTypePipe
+    FilterByTypePipe,
+    HoverAffectDirective
   ],
   imports: [
     BrowserModule
